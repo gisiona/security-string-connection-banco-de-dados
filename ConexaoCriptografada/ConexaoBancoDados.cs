e4507e3c57bf55ecf19d3143f50b257fb2175e52
Gisiona <gil-real@hotmail.com>
2014-11-30 18:16:17 -0200

    Projeto de criptografia de connection string

---
+++ ConexaoCriptografada/ConexaoBancoDados.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Configuration;
+
+namespace ConexaoCriptografada
+{
+    public class ConexaoBancoDados
+    {
+        /// <summary>
+        /// Metodo para criptografar a string de conexao 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public string EncryptConnectionString(string connectionString)
+        {
+            Byte[] b = System.Text.ASCIIEncoding.ASCII.GetBytes(connectionString);
+            string encryptedConnectionString = Convert.ToBase64String(b);
+            return encryptedConnectionString;
+        }
+
+       /// <summary>
+       /// Metodo que descriptografa a string de conexao
+       /// </summary>
+       /// <returns></returns>
+        public string DecryptConnectionString()
+        {
+            Byte[] b = Convert.FromBase64String(ConfigurationSettings.AppSettings["connectionString"]);
+            string decryptedConnectionString = System.Text.ASCIIEncoding.ASCII.GetString(b);
+            return decryptedConnectionString;
+        }
+    }
+}
---
+++ CriptografarDescriptografarDados/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace CriptografarDescriptografarDados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                ConexaoCriptografada.ConexaoBancoDados cs = new ConexaoCriptografada.ConexaoBancoDados();
+                conn = new SqlConnection(cs.DecryptConnectionString());
+                conn.Open();
+                MessageBox.Show("Conectado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ConexaoCriptografada.ConexaoBancoDados cs = new ConexaoCriptografada.ConexaoBancoDados();
+            txtDescriptografar.Text = txtCriptografar.Text;
+            txtCriptografar.Text = cs.EncryptConnectionString(txtCriptografar.Text);
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(txtDescriptografar.Text.Length != 0)
+            {
+                ConexaoCriptografada.ConexaoBancoDados cs = new ConexaoCriptografada.ConexaoBancoDados();
+                txtCriptografar.Text = txtDescriptografar.Text;
+                txtDescriptografar.Text = cs.DecryptConnectionString();
+            }
+        }
+    }
+}
